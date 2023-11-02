9ee85ba4a4905a960c6aea0b55ad52ec1f8fb3cd
francisco <francis_oli92@hotmail.com>
2023-11-02 16:56:54 -0300

    1er commit
    interrupcion del touch anda,hay q añadir el contador global

---
+++ Debug/gpio_ex2_inputCapture.d
@@ -0,0 +1,141 @@
+# FIXED
+
+gpio_ex2_inputCapture.obj: ../gpio_ex2_inputCapture.c
+gpio_ex2_inputCapture.obj: D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/driverlib.h
+gpio_ex2_inputCapture.obj: D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/inc/hw_memmap.h
+gpio_ex2_inputCapture.obj: D:/ti/ccs1120/ccs/ccs_base/msp430/include/msp430.h
+gpio_ex2_inputCapture.obj: D:/ti/ccs1120/ccs/ccs_base/msp430/include/msp430fr4133.h
+gpio_ex2_inputCapture.obj: D:/ti/ccs1120/ccs/ccs_base/msp430/include/in430.h
+gpio_ex2_inputCapture.obj: D:/ti/ccs1120/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+gpio_ex2_inputCapture.obj: D:/ti/ccs1120/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+gpio_ex2_inputCapture.obj: D:/ti/ccs1120/ccs/ccs_base/msp430/include/msp430fr2xx_4xxgeneric.h
+gpio_ex2_inputCapture.obj: D:/ti/ccs1120/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdint.h
+gpio_ex2_inputCapture.obj: D:/ti/ccs1120/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h
+gpio_ex2_inputCapture.obj: D:/ti/ccs1120/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h
+gpio_ex2_inputCapture.obj: D:/ti/ccs1120/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_stdint40.h
+gpio_ex2_inputCapture.obj: D:/ti/ccs1120/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/stdint.h
+gpio_ex2_inputCapture.obj: D:/ti/ccs1120/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/cdefs.h
+gpio_ex2_inputCapture.obj: D:/ti/ccs1120/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_types.h
+gpio_ex2_inputCapture.obj: D:/ti/ccs1120/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_types.h
+gpio_ex2_inputCapture.obj: D:/ti/ccs1120/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_stdint.h
+gpio_ex2_inputCapture.obj: D:/ti/ccs1120/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_stdint.h
+gpio_ex2_inputCapture.obj: D:/ti/ccs1120/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdbool.h
+gpio_ex2_inputCapture.obj: D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/rom_driverlib.h
+gpio_ex2_inputCapture.obj: D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/rom_map_driverlib.h
+gpio_ex2_inputCapture.obj: D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/lcd_e.h
+gpio_ex2_inputCapture.obj: D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/eusci_b_i2c.h
+gpio_ex2_inputCapture.obj: D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/ecomp.h
+gpio_ex2_inputCapture.obj: D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/cs.h
+gpio_ex2_inputCapture.obj: D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/sac.h
+gpio_ex2_inputCapture.obj: D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/timer_b.h
+gpio_ex2_inputCapture.obj: D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/framctl.h
+gpio_ex2_inputCapture.obj: D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/eusci_a_spi.h
+gpio_ex2_inputCapture.obj: D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/eusci_a_uart.h
+gpio_ex2_inputCapture.obj: D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/tia.h
+gpio_ex2_inputCapture.obj: D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/tlv.h
+gpio_ex2_inputCapture.obj: D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/timer_a.h
+gpio_ex2_inputCapture.obj: D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/pmm.h
+gpio_ex2_inputCapture.obj: D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/sysctl.h
+gpio_ex2_inputCapture.obj: D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/sfr.h
+gpio_ex2_inputCapture.obj: D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/mpy32.h
+gpio_ex2_inputCapture.obj: D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/wdt_a.h
+gpio_ex2_inputCapture.obj: D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/icc.h
+gpio_ex2_inputCapture.obj: D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/gpio.h
+gpio_ex2_inputCapture.obj: D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/eusci_b_spi.h
+gpio_ex2_inputCapture.obj: D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/adc.h
+gpio_ex2_inputCapture.obj: D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/crc.h
+gpio_ex2_inputCapture.obj: D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/rtc.h
+gpio_ex2_inputCapture.obj: ../Board.h
+
+../gpio_ex2_inputCapture.c:
+
+D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/driverlib.h:
+
+D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/inc/hw_memmap.h:
+
+D:/ti/ccs1120/ccs/ccs_base/msp430/include/msp430.h:
+
+D:/ti/ccs1120/ccs/ccs_base/msp430/include/msp430fr4133.h:
+
+D:/ti/ccs1120/ccs/ccs_base/msp430/include/in430.h:
+
+D:/ti/ccs1120/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+
+D:/ti/ccs1120/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+
+D:/ti/ccs1120/ccs/ccs_base/msp430/include/msp430fr2xx_4xxgeneric.h:
+
+D:/ti/ccs1120/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdint.h:
+
+D:/ti/ccs1120/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h:
+
+D:/ti/ccs1120/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h:
+
+D:/ti/ccs1120/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_stdint40.h:
+
+D:/ti/ccs1120/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/stdint.h:
+
+D:/ti/ccs1120/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/cdefs.h:
+
+D:/ti/ccs1120/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_types.h:
+
+D:/ti/ccs1120/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_types.h:
+
+D:/ti/ccs1120/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_stdint.h:
+
+D:/ti/ccs1120/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_stdint.h:
+
+D:/ti/ccs1120/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdbool.h:
+
+D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/rom_driverlib.h:
+
+D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/rom_map_driverlib.h:
+
+D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/lcd_e.h:
+
+D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/eusci_b_i2c.h:
+
+D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/ecomp.h:
+
+D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/cs.h:
+
+D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/sac.h:
+
+D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/timer_b.h:
+
+D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/framctl.h:
+
+D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/eusci_a_spi.h:
+
+D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/eusci_a_uart.h:
+
+D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/tia.h:
+
+D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/tlv.h:
+
+D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/timer_a.h:
+
+D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/pmm.h:
+
+D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/sysctl.h:
+
+D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/sfr.h:
+
+D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/mpy32.h:
+
+D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/wdt_a.h:
+
+D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/icc.h:
+
+D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/gpio.h:
+
+D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/eusci_b_spi.h:
+
+D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/adc.h:
+
+D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/crc.h:
+
+D:/DIGI_III_Workspace/test/driverlib/MSP430FR2xx_4xx/rtc.h:
+
+../Board.h:
+
